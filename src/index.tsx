aff3b2a5380307a70d4ec2c05e6fce779ee66f70
U1927 <64768873+U1927@users.noreply.github.com>
2025-12-09 22:03:17 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+console.log("[Index] Application Starting...");
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
+  console.error("[Index] FATAL: Could not find root element!");
   throw new Error("Could not find root element to mount to");
 }
 
+console.log("[Index] Mounting React Root...");
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
